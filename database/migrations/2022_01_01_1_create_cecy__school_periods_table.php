7d0c1ba3f7ee972a469af7ef03f3561cfd444893
J3niff3r Guañuna <jeny.guanuna@gmail.com>
2022-01-05 11:20:50 -0500

    'school_periods'

---
+++ database/migrations/2022_01_01_1_create_cecy__school_periods_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCecySchoolPeriodsTable extends Migration
+{
+    public function up()
+    {
+        Schema::connection(env('DB_CONNECTION_CECY'))->create('school_periods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreignId('state_id')
+                ->nullable()
+                ->constrained('cecy.catalogues'); 
+
+            $table->string('code')->unique()
+                ->comment('codigo unico de periodos lectivos');
+            
+            $table->date('end_date')
+                ->nullable()
+                ->comment('dia final del perido lectivo');
+
+            $table->date('especial_end_date')
+                ->nullable()
+                ->comment('');
+
+            $table->date('especial_start_date')
+                ->nullable()
+                ->comment('');
+
+            $table->date('extraordinary_end_date')
+                ->nullable()
+                ->comment('Para obtener el codido de pais');
+
+            $table->date('extraordinary_start_date')
+                ->nullable()
+                ->comment('Para obtener el codido de pais');
+
+            $table->string('name')
+                ->comment('nombre del periodo lectivo');
+
+            $table->date('ordinary_end_date')
+                ->nullable()
+                ->comment('fin del periodo lectivo');
+
+            $table->date('ordinary_state_date')
+                ->nullable()
+                ->comment('');
+
+            $table->date('start_date')
+                ->nullable()
+                ->comment('');
+
+            
+        });
+    }
+
+    public function down()
+    {
+        Schema::connection(env('DB_CONNECTION_CECY'))->dropIfExists('school_periods');
+    }
+}
