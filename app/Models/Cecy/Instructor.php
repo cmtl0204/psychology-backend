b7fa70974a261053ac71bdc74c78c55b6c38737b
Francis9817 <franciscvalladares@gmail.com>
2022-01-06 14:34:51 -0500

    add model

---
+++ app/Models/Cecy/Instructor.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models\Cecy;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as Auditing;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Instructor extends Model implements Auditable
+{
+    use HasFactory;
+    use Auditing;
+    use SoftDeletes;
+
+    protected $table = 'cecy.instructors';
+
+    protected $fillable = [
+        
+    ];
+
+    // Relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Instructor::class, 'parent_id','core.catalogues');
+    }
+
+    // Mutators
+    
+    // Scopes
+   
+
+    public function scopeCustomOrderBy($query, $sorts)
+    {
+        if (!empty($sorts[0])) {
+            foreach ($sorts as $sort) {
+                $field = explode('-', $sort);
+                if (empty($field[0]) && in_array($field[1], $this->fillable)) {
+                    $query = $query->orderByDesc($field[1]);
+                } else if (in_array($field[0], $this->fillable)) {
+                    $query = $query->orderBy($field[0]);
+                }
+            }
+            return $query;
+        }
+    }
+}
