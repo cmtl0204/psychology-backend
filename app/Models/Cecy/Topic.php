402a68a5afe6831b26fd6bac3c8798f278fc7bd4
Henrry Alvarado <princedark393@gmail.com>
2022-01-06 11:33:03 -0500

    Model  prerequisites and topics

---
+++ app/Models/Cecy/Topic.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models\Cecy;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as Auditing;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Topic extends Model implements Auditable
+{
+    use HasFactory;
+    use Auditing;
+    use SoftDeletes;
+
+    protected $table = 'cecy.topics';
+
+    protected $fillable = [
+        'description',
+    ];
+
+    // Relationships
+    public function parent()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+    public function type()
+    {
+        return $this->belongsTo(Catalogue::class,  'type_id','core.catalogues');
+    }
+
+    // Mutators
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['description'] = strtoupper($value);
+    }
+
+    // Scopes
+    public function scopeDescription($query, $description)
+    {
+        if ($description) {
+            return $query->where('description', $description);
+        }
+    }
+
+    public function scopeCustomOrderBy($query, $sorts)
+    {
+        if (!empty($sorts[0])) {
+            foreach ($sorts as $sort) {
+                $field = explode('-', $sort);
+                if (empty($field[0]) && in_array($field[1], $this->fillable)) {
+                    $query = $query->orderByDesc($field[1]);
+                } else if (in_array($field[0], $this->fillable)) {
+                    $query = $query->orderBy($field[0]);
+                }
+            }
+            return $query;
+        }
+    }
+}
