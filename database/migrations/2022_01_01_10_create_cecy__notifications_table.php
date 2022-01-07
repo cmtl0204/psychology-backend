fe301c4229e1712b29022b2d69c7776acf19b68a
cmtl0204 <cesar.tamayo0204@gmail.com>
2022-01-07 15:01:46 -0500

    fix bugs notifications

--- database/migrations/2022_01_01_10_create_cecy__notifications_table.php
+++ database/migrations/2022_01_01_10_create_cecy__notifications_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCecyNotificationTable extends Migration
+class CreateCecyNotificationsTable extends Migration
 {
     public function up()
     {
-        Schema::connection(env('DB_CONNECTION_CECY'))->create('notification', function (Blueprint $table) {
+        Schema::connection(env('DB_CONNECTION_CECY'))->create('notifications', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->softDeletes();
@@ -29,8 +29,8 @@ class CreateCecyNotificationTable extends Migration
             $table->string('activity_name')
                 ->nullable()
                 ->comment('Descripción del tema de la notificacion');
-                
-            $table->date('notification_date')
+
+            $table->date('registered_at')
                 ->nullable()
                 ->comment('Fecha de la notificación');
         });
@@ -38,6 +38,6 @@ class CreateCecyNotificationTable extends Migration
 
     public function down()
     {
-        Schema::connection(env('DB_CONNECTION_CECY'))->dropIfExists('notification');
+        Schema::connection(env('DB_CONNECTION_CECY'))->dropIfExists('notifications');
     }
 }
