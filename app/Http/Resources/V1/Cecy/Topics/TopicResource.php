b2b96bd9b5728c0a9a7ba424d37380bbfb93e4b6
Henrry Alvarado <princedark393@gmail.com>
2022-01-10 18:42:07 -0500

    Prerequisites

--- app/Http/Resources/V1/Cecy/Topics/TopicResource.php
+++ app/Http/Resources/V1/Cecy/Topics/TopicResource.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Http\Resources\V1\Cecy\Topics;
+namespace App\Http\Resources\V1\Cecy\Prerequisites;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TopicResource extends JsonResource
+class PrerequisiteResource extends JsonResource
 {
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'parent' => TopicResource::make($this->parent),
-            'level' => CatalogueResource::make($this->level),
-            'description' => $this->description,
-        ];
+        // return [
+        //     'id' => $this->id,
+        //     'parent' => TopicResource::make($this->parent),
+        //     'level' => CatalogueResource::make($this->level),
+        //     'description' => $this->description,
+        // ];
     }
 }
