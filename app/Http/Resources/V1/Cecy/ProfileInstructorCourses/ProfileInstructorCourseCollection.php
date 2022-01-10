3ad6777ff04c5cdb13d5ac5116a6d90907533d4f
sebastiansalazar1997 <isb.salazar@yavirac.edu.ec>
2022-01-10 12:55:26 -0500

    Mi mensaje

---
+++ app/Http/Resources/V1/Cecy/ProfileInstructorCourses/ProfileInstructorCourseCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Resources\V1\Cecy\ProfileInstructorCourses;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AuthorityCollection extends ResourceCollection
+{
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection
+        ];
+    }
+}
