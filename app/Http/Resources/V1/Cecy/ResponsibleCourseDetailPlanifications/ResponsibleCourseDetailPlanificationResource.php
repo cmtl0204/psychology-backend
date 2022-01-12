a4ec5cce2ad0411ebaf9c225672be4db7eb33775
bryanPerez <bam.perez@yavirac.edu.ec>
2022-01-12 17:56:52 -0500

    Fix resource

--- app/Http/Resources/V1/Cecy/ResponsibleCourseDetailPlanifications/ResponsibleCourseDetailPlanificationResource.php
+++ app/Http/Resources/V1/Cecy/ResponsibleCourseDetailPlanifications/ResponsibleCourseDetailPlanificationResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources\V1\Cecy\DetailPlanifications;
 
-use App\Models\Cecy\Instructor;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\V1\Cecy\Catalogues\CatalogueResource;
+use App\Http\Resources\V1\Cecy\Classrooms\ClassroomResource;
+use App\Models\Cecy\Instructor;
 
 class ResponsibleCourseDetailPlanificationResource extends JsonResource
 {
@@ -14,10 +16,10 @@ class ResponsibleCourseDetailPlanificationResource extends JsonResource
             'classroom' => ClassroomResource::make($this->classroom),
             'day' => CatalogueResource::make($this->day),
             'workday' => CatalogueResource::make($this->workday),
-            'endTime' => $this->endTime,
             'instructors'=> Instructor::make($this->instructors),
-            'endedAt' => $this->endedAt,
             'daysNumber' => $this->daysNumber,
+            'endTime' => $this->endTime,
+            'endedAt' => $this->endedAt,
             'startTime' => $this->startTime,
             'startedAt' => $this->startedAt,
         ];
