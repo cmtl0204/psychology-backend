79a086484e43a92f0a6ce9d1a59b5053bba898c2
RusticDeveloper <ddc.rivas@yavirac.edu.ec>
2022-01-12 12:11:11 -0500

    changes in RivasController

--- app/Http/Controllers/V1/Cecy/RivasController.php
+++ app/Http/Controllers/V1/Cecy/RivasController.php
@@ -3,20 +3,9 @@
 namespace App\Http\Controllers\V1\Cecy;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\Core\Catalogues\IndexCatalogueRequest;
-use App\Http\Requests\V1\Core\Files\DestroysFileRequest;
-use App\Http\Requests\V1\Core\Files\IndexFileRequest;
-use App\Http\Requests\V1\Core\Files\UpdateFileRequest;
-use App\Http\Requests\V1\Core\Files\UploadFileRequest;
-use App\Http\Resources\V1\Core\Catalogues\CatalogueCollection;
-use App\Models\Authentication\User;
-use App\Models\Cecy\AdditionalInformation;
-use App\Models\Cecy\Catalogue as CecyCatalogue;
 use App\Models\Cecy\Participant;
-use App\Models\Core\Catalogue;
-use App\Models\Core\File;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 class ParticipantController extends Controller
 {
