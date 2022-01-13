8fd31173ebe1460ebfe9033423b1e77b4be78b85
Erick-Damian-Casis <erickdc.lol@gmail.com>
2022-01-13 11:58:14 -0500

    create resources

--- app/Http/Controllers/V1/Cecy/DamianController.php
+++ app/Http/Controllers/V1/Cecy/DamianController.php
@@ -5,24 +5,22 @@ namespace App\Http\Controllers\V1\Cecy;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Cecy\Registrations\StoreRegistrationRequest;
 use App\Http\Resources\V1\Cecy\Registrations\RegistrationResource;
-use App\Models\Cecy\AdditionalInformation;
+use App\Models\Cecy\Catalogue;
 use App\Models\Cecy\Participant;
 use App\Models\Cecy\Registration;
-use Illuminate\Http\Request;
 
 class DamianController extends Controller
 {
     //inscripcion al curso
-    public function storeRegistration(StoreRegistrationRequest $request)
+    public function registerStudent(StoreRegistrationRequest $request)
     {
-        $registration = new Participant();
+        $registration = new Registration();
 
-        // necesito el id del participante
         $registration->participant()
             ->associate(Participant::find($request->input('participant.id')));
 
         $registration->type()
-            ->associate(Participant::find($request->input('type.id')));
+            ->associate(Catalogue::find($request->input('type.id')));
 
         $registration->number = $request->input('number');
         $registration->registered_at = $request->input('registeredAt');
