1a6a73861503b44330af69577f86a4c31af0fa62
khabLexander <mantillagka@hotmail.com>
2022-01-19 02:50:34 -0500

    Added planifaciotns resoruces

--- app/Http/Resources/V1/Cecy/Planifications/PlanificationResource.php
+++ app/Http/Resources/V1/Cecy/Planifications/PlanificationResource.php
@@ -10,7 +10,19 @@ class PlanificationResource extends JsonResource
     public function toArray($request)
     {
         return [
-            
+            'id' => $this->id,
+            'area' => CatalogueResource::make($this->area),
+            'course' => CourseResource::make($this->course),
+            'responsibleCourse' => InstructorResource::make($this->responsibleCourse),
+            'responsibleCecy' => AuthorityResource::make($this->responsibleCecy),
+            'schoolPeriod' =>SchoolPeriodResource::make($this->schoolPeriod),
+            'sector' => CatalogueResource::make($this->sector),
+            'state' => CatalogueResource::make($this->state),
+            'code' => $this->code,
+            'endedAt' => $this->ended_at,
+            'needs' => $this->needs,
+            'numberDays' => $this->number_days,
+            'startedAt' => $this->started_at,
         ];
     }
 }
