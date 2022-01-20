a11b230264cc00bd658e3364a833997a74c28fd0
fercho2798 <ferchofercho933@gmail.com>
2022-01-20 10:29:14 -0500

    avanced subproceso 10

---
+++ app/Http/Resources/V1/Cecy/DetailPlanifications/DetailPlanificationInformNeedResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\V1\Cecy\DetailPlanificationInformNeeds;
+
+use App\Http\Resources\V1\Cecy\DetailInstructors\DetailInstructorResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DetailPlanificationInformNeedResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'day' => CatalogueResource::make($this->day),
+            'endTime' => $this->end_time,
+            'startTime' => $this->start_time,
+            'planificacion' => PlanificationInformNeedResource::collection($this->planification),
+            'classroom' => ClassroomInformNeedResource::collection($this->classroom)
+        ];
+    }
+}
