336436802f2bddfef006bfd3b3043fe3e3064153
khabLexander <mantillagka@hotmail.com>
2022-01-20 08:46:18 -0500

    No borren :c

--- app/Http/Controllers/V1/Cecy/GuachagmiraController.php
+++ app/Http/Controllers/V1/Cecy/GuachagmiraController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\V1\Cecy;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\Cecy\Courses\getCoursesByCategoryRequest;
-use App\Http\Requests\V1\Cecy\Courses\getCoursesByNameRequest;
-use App\Http\Requests\V1\Cecy\Courses\getInstructorsInformationByCourseRequest;
-use App\Http\Requests\V1\Cecy\Courses\getSchedulesInformationByCourseRequest;
+use App\Http\Requests\V1\Cecy\Courses\GetCoursesByCategoryRequest;
+use App\Http\Requests\V1\Cecy\Courses\GetCoursesByNameRequest;
+use App\Http\Requests\V1\Cecy\Courses\GetInstructorsInformationByCourseRequest;
+use App\Http\Requests\V1\Cecy\Courses\GetSchedulesInformationByCourseRequest;
 use App\Http\Resources\V1\Cecy\Courses\CourseCollection;
 use App\Models\Cecy\Course;
 use App\Models\Core\File;
@@ -52,10 +52,10 @@ class GuachagmiraController extends Controller
                 ]
             ]);
     }
-    public function getInstructorsInformationByCourse(getInstructorsInformationByCourseRequest $request)
+    public function getInstructorsInformationByCourse(GetInstructorsInformationByCourseRequest $request)
     {
     }
-    public function getSchedulesInformationByCourse(getSchedulesInformationByCourseRequest $request)
+    public function getSchedulesInformationByCourse(GetSchedulesInformationByCourseRequest $request)
     {
     }
 
