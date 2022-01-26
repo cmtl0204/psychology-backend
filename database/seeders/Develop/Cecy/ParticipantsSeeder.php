55b7051e1af1151af9d2041efd88b2286f7e4e63
khabLexander <mantillagka@hotmail.com>
2022-01-26 12:08:54 -0500

    Fixing catalogue

--- database/seeders/Develop/Cecy/ParticipantsSeeder.php
+++ database/seeders/Develop/Cecy/ParticipantsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders\Cecy;
 
+use App\Models\Cecy\Catalogue;
 use Illuminate\Database\Seeder;
 
 class ParticipantsSeeder extends Seeder
@@ -13,6 +14,34 @@ class ParticipantsSeeder extends Seeder
      */
     public function run()
     {
-        //CREAR AQUI LAS SEMILLAS PARA PARTICIPANTS
+        $this->createParticipantCatalogue();
+        $this->createParticipants();
+    }
+    public function createParticipantCatalogue()
+    {
+        $catalogue = json_decode(file_get_contents(storage_path() . "/catalogue.json"), true);
+        Catalogue::factory()->sequence(
+            [
+                'code' => $catalogue['participant_state']['approved'],
+                'name' => 'Aprobado',
+                'type' => $catalogue['participant_state']['type'],
+                'description' => 'Estado del estudiante de aprobado en el curso'
+            ],
+            [
+                'code' => $catalogue['participant_state']['not_approved'],
+                'name' => 'Reprobado',
+                'type' => $catalogue['participant_state']['type'],
+                'description' => 'Estado del estudiante cuando esta esperando ser aprobado'
+            ],
+            [
+                'code' => $catalogue['participant_state']['to_be_approved'],
+                'name' => 'Por aprobar',
+                'type' => $catalogue['participant_state']['type'],
+                'description' => 'Estado del estudiante de reprobado en el curso'
+            ]
+        )->create();
+    }
+    public function createParticipants()
+    {
     }
 }
