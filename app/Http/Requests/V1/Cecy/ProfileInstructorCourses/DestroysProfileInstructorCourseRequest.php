41fb89d98ba15eb2a69a4195ffdb6222c124d6c1
bryanPerez <bam.perez@yavirac.edu.ec>
2022-01-31 19:29:22 -0500

    Fix course profile' files

---
+++ app/Http/Requests/V1/Cecy/ProfileInstructorCourses/DestroysProfileInstructorCourseRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\V1\Cecy\Courses;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DestroysProfileInstructorCourseRequest extends FormRequest
+{
+  public function authorize()
+  {
+    return true;
+  }
+  public function rules()
+  {
+    return [
+      'ids' => ['required'],
+    ];
+  }
+
+  public function attributes()
+  {
+    return [
+      'ids' => 'ID`s de las aulas',
+    ];
+  }
+}
--- app/Models/Cecy/ProfileInstructorCourse.php
+++ app/Models/Cecy/ProfileInstructorCourse.php
@@ -17,9 +17,9 @@ class ProfileInstructorCourse extends Model implements Auditable
     protected $table = 'cecy.profile_instructor_courses';
 
     protected $fillable = [
-        'require_experience',
-        'require_knowledge',
-        'require_skills',
+        'requires_experiences',
+        'requires_knowledges',
+        'requires_skills',
     ];
 
     // Relationships
@@ -27,9 +27,10 @@ class ProfileInstructorCourse extends Model implements Auditable
     {
         return $this->belongsTo(Course::class);
     }
+
     public function instructors()
     {
-        return $this->belongsToMany(Instructor::class, 'authorized_instructors', 'instructor_id', 'profile_instructor_id');
+        return $this->belongsToManyy(Instructor::class, 'authorized_instructors', 'instructor_id', 'profile_instructor_id');
     }
     // Mutators
 
@@ -52,6 +53,13 @@ class ProfileInstructorCourse extends Model implements Auditable
                 }
             }
             return $query;
+        }
+    }
+
+    public function scopeCourse($query, $profile)
+    {
+        if ($profile) {
+            return $query->Where('course_id', $profile->course);
         }
     }
 }
