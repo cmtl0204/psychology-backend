2dcb66d989bb4b46bd06074d24209c6d36e5948e
1727326520 <ferchofercho933@gmail.com>
2022-02-02 08:24:23 -0500

    changes

--- database/seeders/Develop/Cecy/AuthorizedInstructorsSeeder.php
+++ database/seeders/Develop/Cecy/AuthorizedInstructorsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders\Develop\Cecy;
 
+use App\Models\Cecy\Instructor;
+use App\Models\Cecy\ProfileInstructorCourse;
 use Illuminate\Database\Seeder;
 
 class AuthorizedInstructorsSeeder extends Seeder
@@ -14,6 +16,16 @@ class AuthorizedInstructorsSeeder extends Seeder
     public function run()
     {
         // AuthorizedInstructor::factory()->create();
+
+        $profileInstructorCourses = ProfileInstructorCourse::get();
+        $instructors = Instructor::get();
+        foreach ($instructors as $instructor) {
+            foreach ($profileInstructorCourses as $profileInstructorCourse) {
+                $instructor->profileInstructorCourses()->attach(
+                    $profileInstructorCourse->id
+                );
+            }
+        }
     }
 
 }
